b296d556d057a0bc4440fcaeafe4d316faf5464d
Christopher Lacaille <Christopher Lacaille>
2016-08-07 02:30:46 -0500

    update comments

--- intermediate_algorithms/drop_it.js
+++ intermediate_algorithms/drop_it.js
@@ -1,18 +1,18 @@
 function dropElements(arr, func) {
   var newArr;
   
-//   start at second index
+  //   start at second index
   for (var i = 1; i < arr.length; i++) {
-//   if last index is false and current is true - 
-//    return rest of array, not including the last false
+    //   if last index is false and current is true - 
+    //    return rest of array, not including the last false
     if (!func(arr[i-1]) && func(arr[i])) {
       newArr = arr.splice(i);
     } 
-//   If the first item is true - return original array
+    //   If the first item is true - return original array
     else if (func(arr[i-1])) {
       newArr = arr;
     } 
-//  If no conditions are true - return empty array
+    //  If no conditions are true - return empty array
     else {
       return [];
     }
