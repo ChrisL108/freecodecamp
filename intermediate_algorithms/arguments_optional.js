6ded81d1b0720f1e324e057dd1f387fbf47b0c9f
Christopher Lacaille <Christopher Lacaille>
2016-08-07 02:33:30 -0500

    update comments

--- intermediate_algorithms/arguments_optional.js
+++ intermediate_algorithms/arguments_optional.js
@@ -3,14 +3,14 @@ function addTogether(a, b) {
     //   check if 'a' (param) is a number 
     if (+a !== a) return undefined;
 
-    // if 'b' parameter was provided, check
+    // if 'b' (param) was provided, check
     // that it's a number, then return sum of a + b
     if (b) {
         if(+b !== b) return undefined;
         return a + b;
     }
 
-    // if just 'a' parameter given, return function
+    // if just 'a' (param) given, return function
     return function(b) {
         if(+b !== b) return undefined;
         return a + b;
